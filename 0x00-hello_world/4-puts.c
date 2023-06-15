16b6c985c65b4cb5a26f0de29aaf3b6a1c527592
lochodi <michaelwangari123@gmail.com>
2023-06-15 08:59:13 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stuio.h>
+/**
+ * main - A program that prints a line with put function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
