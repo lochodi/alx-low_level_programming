f504dba593cdeedce46efa9427356d9c6082fe6d
lochodi <michaelwangari123@gmail.com>
2023-06-22 12:10:17 -0700

    update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
 /**
-* main - Test function for positive or negative
-* Return: 0
+* print_sign - prints the sign of a number
+* @n: the int to check
+* Return: 1 and prints + if n is greater than zero
+* 0 and prints 0 if n is zero
+* -1 and prints - if n is less than zero
 */
 
-int main(void)
+int print_sign(int n)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+if (n > 0)
+{
+_putchar('+');
+return (1);
 }
+else if (n == 0)
+{
