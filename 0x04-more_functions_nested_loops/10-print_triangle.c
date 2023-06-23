26ce4a707d278a1fd4e85672e9a4a0c085544ab1
lochodi <michaelwangari123@gmail.com>
2023-06-22 18:07:32 -0700

    update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,20 +11,17 @@ if (size <= 0)
 _putchar('\n');
 } else
 {
- int i, j;
-
+int i, j;
 for (i = 1; i <= size; i++)
 {
 for (j = i; j < size; j++)
 {
 _putchar(' ');
 }
-
 for (j = 1; j <= i; j++)
 {
 _putchar('#');
 }
-
 _putchar('\n');
 }
 } 
