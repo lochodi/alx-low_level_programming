ae8d2b06e4951b98402aafefd7e856a3666c38b2
lochodi <michaelwangari123@gmail.com>
2023-06-23 07:29:04 -0700

    update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,13 +3,15 @@
 /**
  * print_triangle - prints a triangle, followed by a new line
  * @size: size of the triangle
+ * Return: void
  */
 void print_triangle(int size)
 {
 if (size <= 0)
 {
 _putchar('\n');
-} else
+}
+else
 {
 int i, j;
 for (i = 1; i <= size; i++)
@@ -24,6 +26,6 @@ _putchar('#');
 }
 _putchar('\n');
 }
-} 
+}
 }
 
