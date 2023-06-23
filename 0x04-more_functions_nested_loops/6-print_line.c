8bb3aff180c8913297d2ce5418b53ab5fc817a39
lochodi <michaelwangari123@gmail.com>
2023-06-23 06:56:29 -0700

    update

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,25 +3,25 @@
 /**
  * print_line - Draws a straight line according to parameter
  * @n: The number of times to draw
- * Return: empty
+ * Return: void
  */
 
 void print_line(int n)
 
 {
 	
-	int x;
+int x;
 
-	if (n <= 0)
-	{
-	_putchar('\n');
-	}
-	else
-	{
-	for (x = 0; x < n; x++)
-	{
-	_putchar(95);
-	}
-	_putchar('\n');
-	}
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 0; x < n; x++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
 }	
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,16 +1,15 @@
 #include "main.h"
-
 /**
  * print_diagonal - draws a diagonal line on the terminal
  * @n: number of times the character \ should be printed
  */
 void print_diagonal(int n)
-
 {
 if (n <= 0)
 {
 _putchar('\n');
-} else
+} 
+else
 {
 int i, j;
 
@@ -27,4 +26,3 @@ _putchar('\n');
 }
 }
 }
-
